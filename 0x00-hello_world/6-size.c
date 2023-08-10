fb410c81625ddb073e3fa55e9c2818dfa9da1411
robsenshemsedin <shemsedinrobsen@gmail.com>
2023-08-10 12:15:34 -0700

    Added a parantesis on the return statement

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,5 +17,5 @@ printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(ll));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-return (0)
+return (0);
 }
