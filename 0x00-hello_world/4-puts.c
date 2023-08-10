c3fdb7c69d43eeb479d198771db2c4542cfa6d40
robsenshemsedin <shemsedinrobsen@gmail.com>
2023-08-10 11:18:00 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * A main function that prints \"Programming is like building a multilingual puzzl  and ends with new line
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
+}
