7eadf411655eaa9b968c1507a52351550205db56
robsenshemsedin <shemsedinrobsen@gmail.com>
2023-08-15 10:23:32 -0700

    Correction on 3-islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -9,9 +9,9 @@ int _islower(int c)
 {
 	int value;
 
-	if ((c + '0') >= 'a' && (c + '0') <= 'z')
-		value = 0;
-	else
+	if (c >= 'a' && c <= 'z')
 		value = 1;
+	else
+		value = 0;
 	return (value);
 }
