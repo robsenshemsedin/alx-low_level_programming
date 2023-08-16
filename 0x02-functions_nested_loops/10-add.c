78830c7c4c5a5732aa03efc783c5727d22d74d6a
robsenshemsedin <shemsedinrobsen@gmail.com>
2023-08-16 01:04:35 -0700

    Add two number

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - Add two numbers.
+ * @num1: first number.
+ * @num2: second number.
+ *
+ * Return: retun the sum.
+ */
+int add(int num1, int num2)
+{
+	int sum = num1 + num2;
+
+	return (sum);
+}
