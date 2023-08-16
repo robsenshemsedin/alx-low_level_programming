7dbde2367c2ef0635345927529fcda44365f4196
robsenshemsedin <shemsedinrobsen@gmail.com>
2023-08-16 09:28:17 -0700

    Using unsigned long to print the fist 78 fibonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#define LIMIT 98
+
+/**
+ * main - Printing the farst 98 fibonacci numbers.
+ *
+ * Return: Always return 0.
+ */
+int main(void)
+{
+	unsigned long num1 = 0;
+	unsigned long num2 = 1;
+	unsigned long i = 1;
+
+	while (i <= LIMIT)
+	{
+		unsigned long sum = 0;
+
+		sum = num1 + num2;
+		if (i == LIMIT)
+			printf("%lu", sum);
+		else
+			printf("%lu, ", sum);
+		num1 = num2;
+		num2 = sum;
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
