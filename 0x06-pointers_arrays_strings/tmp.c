1e157d00396dc9269683d97eba2f530a7f348e35
robsenshemsedin <shemsedinrobsen@gmail.com>
2023-08-25 08:25:38 -0700

    Added the 1-strncat.c

---
+++ 0x06-pointers_arrays_strings/tmp.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * _strncat - Concatenates atmost n elements of the second srcing to
+ * the fist stirng. 
+ * @dest: Pointer to the first character of the first srcing.
+ * @src: Pointer to the first character of the second srcing.
+ * @n: Maximum number of characters to be concatenated.
+ *
+ * Return: Returns pointer to the first character fo the first input srcing.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	char tmp = 'a';
+	char *ptr = &tmp;
+	int i = 1;
+
+	ptr = dest;
+	while (1)
+	{
+		if (*dest == '\0')
+		{
+			while ((*src != '\0') && (i <= n))
+			{
+				*dest = *src;
+				++dest;
+				++src;
+				++i;
+			}
+			if (i > n)
+			{
+				while (1)
+				{
+					if (*src == '\0')
+					{
+						*src = 'a';
+						break;
+					}
+					++src;
+				}
+			}
+			*dest = '\0';
+			break;
+		}
+		++dest;
+	}
+	return (ptr);
+}
